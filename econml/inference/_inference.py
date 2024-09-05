f5a11be0b9581e577872ccfcd501fcc6cc80769f
ssemov <ssemov@gmail.com>
2024-09-05 14:23:36 -0700

    "LinearDRIV shape inconsistency"

--- econml/inference/_inference.py
+++ econml/inference/_inference.py
@@ -13,7 +13,8 @@ from scipy.stats import norm
 from ._bootstrap import BootstrapEstimator
 from ..utilities import (Summary, _safe_norm_ppf, broadcast_unit_treatments,
                          cross_product, inverse_onehot, ndim,
-                         parse_final_model_params, reshape_treatmentwise_effects, shape, filter_none_kwargs)
+                         parse_final_model_params, reshape_treatmentwise_effects, shape, filter_none_kwargs,
+                         reshape_outcomewise_effects)
 
 """Options for performing inference in estimators."""
 
@@ -273,6 +274,7 @@ class LinearModelFinalInference(GenericModelFinalInference):
         return self.effect_inference(X, T0=T0, T1=T1).conf_int(alpha=alpha)
 
     def effect_inference(self, X, *, T0, T1):
+        print("enter effect inference")
         # We can write effect inference as a function of prediction and prediction standard error of
         # the final method for linear models
         X, T0, T1 = self._est._expand_treatments(X, T0, T1)
@@ -281,8 +283,9 @@ class LinearModelFinalInference(GenericModelFinalInference):
         elif self.featurizer is not None:
             X = self.featurizer.transform(X)
         XT = cross_product(X, T1 - T0)
-        e_pred = self._predict(XT)
-        e_stderr = self._prediction_stderr(XT)
+        print(self._predict(XT).shape)
+        e_pred = reshape_outcomewise_effects(self._predict(XT), self._d_y)
+        e_stderr = reshape_outcomewise_effects(self._prediction_stderr(XT), self._d_y)
         d_y = self._d_y[0] if self._d_y else 1
 
         mean_XT = XT.mean(axis=0, keepdims=True)
